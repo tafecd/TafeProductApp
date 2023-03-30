74e7c0607df0bc710ae83a0de4805867b5de5986
Cindy Dennison <Cindy.DENNISON@student.tafesa.edu.au>
2023-03-31 10:11:28 +1030

    as “Commit the branch feat_GST1.2

--- ProductMaintenance/ProductApps/MainWindow.xaml.cs
+++ ProductMaintenance/ProductApps/MainWindow.xaml.cs
@@ -36,6 +36,7 @@ namespace ProductApps
                 totalPaymentTextBlock.Text = Convert.ToString(cProduct.TotalPayment);
                 totalChargeTextBox.Text = Convert.ToString(cProduct.TotalPayment + 25);
                 totalChargeWithWrapTextBox.Text = Convert.ToString(cProduct.TotalPayment + 25 + 5);
+                totalChargeAfterGSTTextBox.Text = Convert.ToString((cProduct.TotalPayment + 25 + 5) * 1.1M);
             }
             catch (FormatException)
             {
